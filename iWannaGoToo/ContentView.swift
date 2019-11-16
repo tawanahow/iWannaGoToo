f7acc40741f44cfd0f3b99b0a2a52effb72933a9
Tawana Howard <tawanahow@gmail.com>
2019-11-16 11:25:14 -0500

    Initial Commit

---
+++ iWannaGoToo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iWannaGoToo
+//
+//  Created by tawana howard on 11/16/19.
+//  Copyright © 2019 tawana howard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
